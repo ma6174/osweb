3bb2c7f9ae7152e1f02e38167f873d3af3d72a71
ma6174 <ma6174@163.com>
2012-11-29 09:04:30 +0800

    关闭调试模式，修改主页

--- osweb.py
+++ osweb.py
@@ -11,6 +11,7 @@ urls = (
     '/test/(.*)','TEST',
 )
 app = web.application(urls,globals())
+web.config.debug = False
 #数据库类
 class OS:
     #将题目保存到数据库
@@ -133,55 +134,58 @@ class TEST():
             TEST.total_time=TEST.total_blank*TEST.time[TEST.level]
             return template.render(time=TEST.total_time,problem=TEST.problem,td=a,num=num)
         except:
-            return '<h1>System Error<br/><br/><a href="/test/">back</a></h1>'
+            return '<h1>System Error<br/><br/><a href="/">back</a></h1>'
 
     #POST方法，获取用户提交的数据并进行处理
     def POST(self,name):
-        try:
-            end_time=datetime.datetime.now()
+#        try:
+        end_time=datetime.datetime.now()
 #            print TEST.start_time,end_time
+        try:
             use=end_time-TEST.start_time
+        except:
+            return '<h1>Please click button at the end of rank page<br/><br/><a href="/">back</a></h1>'
 #            print use.seconds,TEST.total_time
-            if use.seconds-10 > TEST.total_time: #防止禁用JS而使时间停止作弊
-                return "<h1>Please NOT Forbid JS！</h1>"
-            input = web.input()
-            error_pro=[]
-            error_user=[]
-            right_ans=[]
-            for row in range(0,6):#1-6
+        if use.seconds-10 > TEST.total_time: #防止禁用JS而使时间停止作弊
+            return "<h1>Please NOT Forbid JS！</h1>"
+        input = web.input()
+        error_pro=[]
+        error_user=[]
+        right_ans=[]
+        for row in range(0,6):#1-6
 #                if TEST.session==1:     #防止后退以后不刷新而而直接做题
 #                    return "<h1>请刷新页面后再开始做题</h1>"
-                if input["A%s"%row] not in TEST.problem:#判断题目是否在题库中，防止非法提交
-                    return '<h1>Please refresh before test<br/><br/><a href="/test/">back</a></h1>'
-                std_ans,num= self.os.get_answer(input["A%s"%row])
-                user_ans = []
-                user_all_ans = []
-                tag=0
-                for line in map(chr,range(66,71)):#65-71/
-                    a = "%s%s"%(line,row)
-                    user_all_ans.append(input[a])
+            if input["A%s"%row] not in TEST.problem:#判断题目是否在题库中，防止非法提交
+                return '<h1>Please refresh before test<br/><br/><a href="/test/">back</a></h1>'
+            std_ans,num= self.os.get_answer(input["A%s"%row])
+            user_ans = []
+            user_all_ans = []
+            tag=0
+            for line in map(chr,range(66,71)):#65-71/
+                a = "%s%s"%(line,row)
+                user_all_ans.append(input[a])
 #                    print input[a]
-                    #回答正确的标准：输入的信息在正确答案中并且输入的信息不重复
-                    if (input[a] in std_ans) and (input[a] not in user_ans) and input[a]!='':
-                        user_ans.append(input[a])
-                        self.accept += 1
-                        tag+=1
-                #统计出错题目
-                if tag<num and input["A%s"%row] not in error_pro:
-                    error_user.append(user_all_ans)
-                    error_pro.append('%s'%input["A%s"%row])
-                    right_ans.append(std_ans)
-            print "total2",self.total_blank
-            score = int(self.accept*100/TEST.total_blank)#计算成绩
-            print "score=",score
-            self.os.save_score(score)   #保存成绩
-            TEST.session=1
-            env = Environment(loader=PackageLoader('osweb', './'))#成绩页面
-            template = env.get_template('rank.html')
-            num = range(len(error_pro))
-            return template.render(num = num,err_ans=error_user,score=score,rank=self.os.get_rank(score),problem=error_pro,answer=right_ans)
-        except:
-            return '<h1>System Error<br/><br/><a href="/test/">back</a></h1>'
+                #回答正确的标准：输入的信息在正确答案中并且输入的信息不重复
+                if (input[a] in std_ans) and (input[a] not in user_ans) and input[a]!='':
+                    user_ans.append(input[a])
+                    self.accept += 1
+                    tag+=1
+            #统计出错题目
+            if tag<num and input["A%s"%row] not in error_pro:
+                error_user.append(user_all_ans)
+                error_pro.append('%s'%input["A%s"%row])
+                right_ans.append(std_ans)
+        print "total2",self.total_blank
+        score = int(self.accept*100/TEST.total_blank)#计算成绩
+        print "score=",score
+        self.os.save_score(score)   #保存成绩
+        TEST.session=1
+        env = Environment(loader=PackageLoader('osweb', './'))#成绩页面
+        template = env.get_template('rank.html')
+        num = range(len(error_pro))
+        return template.render(num = num,err_ans=error_user,score=score,rank=self.os.get_rank(score),problem=error_pro,answer=right_ans)
+#        except:
+ #           return '<h1>System Error<br/><br/><a href="/">back</a></h1>'
 
 
 #首页
